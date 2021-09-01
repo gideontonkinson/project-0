e57a5887cef0bfbec008bab7f8a53fb3e4cd5001
Gideon Tonkinson <gmtonkinson@gmail.com>
2021-09-01 14:22:47 -0600

    Update

---
+++ project-0-main/cmake-build-debug-schizo/CMakeFiles/project-0-main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/guest/g/gmt27/Book.cpp" "/users/guest/g/gmt27/cmake-build-debug-schizo/CMakeFiles/project-0-main.dir/Book.cpp.o"
+  "/users/guest/g/gmt27/BookCreator.cpp" "/users/guest/g/gmt27/cmake-build-debug-schizo/CMakeFiles/project-0-main.dir/BookCreator.cpp.o"
+  "/users/guest/g/gmt27/BookListCreator.cpp" "/users/guest/g/gmt27/cmake-build-debug-schizo/CMakeFiles/project-0-main.dir/BookListCreator.cpp.o"
+  "/users/guest/g/gmt27/Info.cpp" "/users/guest/g/gmt27/cmake-build-debug-schizo/CMakeFiles/project-0-main.dir/Info.cpp.o"
+  "/users/guest/g/gmt27/Library.cpp" "/users/guest/g/gmt27/cmake-build-debug-schizo/CMakeFiles/project-0-main.dir/Library.cpp.o"
+  "/users/guest/g/gmt27/main.cpp" "/users/guest/g/gmt27/cmake-build-debug-schizo/CMakeFiles/project-0-main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project-0-main/cmake-build-debug-schizo/CMakeFiles/project-0-main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project-0-main.dir/Book.cpp.o"
+  "CMakeFiles/project-0-main.dir/BookCreator.cpp.o"
+  "CMakeFiles/project-0-main.dir/BookListCreator.cpp.o"
+  "CMakeFiles/project-0-main.dir/Info.cpp.o"
+  "CMakeFiles/project-0-main.dir/Library.cpp.o"
+  "CMakeFiles/project-0-main.dir/main.cpp.o"
+  "project-0-main"
+  "project-0-main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project-0-main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
